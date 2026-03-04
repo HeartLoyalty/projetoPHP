2e8d57e8db8be26b189237ba4ad0747da1ed113b
HeartLoyalty <juannytoferreira@gmail.com>
2026-03-03 21:41:35 -0300

    tirst commit

---
+++ segundo.php
@@ -0,0 +1,19 @@
+<?php
+//comentario em linha
+
+/*comentario 
+		em 
+			bloco*/
+
+	$a = 10;
+	$num = 20;
+	$num2 = 60.5;
+	$nome = "Juan ";
+	$sobreNome = 'Lopes';
+	$status =  true;
+	$flag = false;
+
+		$resp =  $a + $b;
+		$respN = $nome + $sobreNome;
+
+ ?>
