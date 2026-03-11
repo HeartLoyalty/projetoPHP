0036e2cafbd8952b89f9e5b2fbb202d767892b7d
HeartLoyalty <juannytoferreira@gmail.com>
2026-03-10 21:13:41 -0300

    complement

--- variaveis.php
+++ variaveis.php
@@ -5,14 +5,17 @@ $cargo = "Estagiario"; 			// string
 $idade = 18; 					//int
 $resultado = false; 				//boolean
 
-if ($resultado == false) {				//condicional true
+if (!$resultado) {				//condicional true
 	print("$data <br/> $salario <br/> $cargo <br/> $idade <br/> $resultado");
 }
 
-echo "Arquivo criado em $data";
+echo "Arquivo criado em $data <br/>" ;
 
-printf("Salario minimo: R$ %.2f <br/>", $salario);
-$texto = sprintf("%s recebe R$ %.2f por mês", $cargo, $salario);
-echo $texto;
+printf("Salario minimo: R$ %.2f <br/>", $salario);  // formatação para numero flutuante.
+$texto = sprintf("%s recebe R$ %.2f por mês", $cargo, $salario * 2 );// formatação para string atribuida a uma variavel.
+
+
+echo $texto; // retornando a variavel formatada.
+
 
  ?>
