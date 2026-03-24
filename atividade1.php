60b4b561b47dadea5d0819d7891ffb2464582996
HeartLoyalty <juannytoferreira@gmail.com>
2026-03-24 19:57:02 -0300

    save

---
+++ atividade1.php
@@ -0,0 +1,38 @@
+<?php 
+	$aluno1;
+	$a1nota1 = 7.0;
+	$a1nota2 = 5.0;
+	$media1 = ($a1nota1 + $a1nota2) / 2
+
+	$aluno2;
+	$a2nota1 = 4.0;
+	$a2nota2 = 3.0;
+	$media2 = ($a2nota1 + $a2nota2) / 2
+
+	$aluno3;
+	$a3nota1 = 8.0;
+	$a3nota2 = 9.0;
+	$media3 = ($a3nota1 + $a3nota2) / 2
+
+	$aluno4;
+	$a4nota1 = 2.0;
+	$a4nota2 = 6.0;
+	$media4 = ($a4nota1 + $a4nota2) / 2
+
+	$aluno5;
+	$a5nota1 = 6.0;
+	$a5nota2 = 10.0;
+	$media5 = ($a5nota1 + $a5nota2) / 2
+	
+	$aluno6;
+	$a6nota1 = 5.0;
+	$a6nota2 = 1.0;
+	$media6 = ($a6nota1 + $a6nota2) / 2
+
+	if (condition) {
+		// code...
+	}
+	
+
+	
+ ?>
