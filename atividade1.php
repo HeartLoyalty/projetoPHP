5fd3e3469b293ecd07280ec20407663b71f9f468
HeartLoyalty <juannytoferreira@gmail.com>
2026-03-24 21:01:07 -0300

    twosave

--- atividade1.php
+++ atividade1.php
@@ -29,10 +29,81 @@
 	$a6nota2 = 1.0;
 	$media6 = ($a6nota1 + $a6nota2) / 2
 
-	if (condition) {
-		// code...
+	if ($media1 < 3.0) {
+		$aluno1 = "reprovado";
+	}else if($media1 < 7.0){
+		$aluno1 "Exame";
+	}else{
+
+		$aluno1 "Aprovado";
 	}
-	
+
+
+
+	if ($media2 < 3.0) {
+		$aluno2 = "reprovado";
+	}else if($media2 < 7.0){
+		$aluno2 "Exame";
+	}else{
+
+		$aluno2 "Aprovado";
+	}
+
+
+
+
+	if ($media3 < 3.0) {
+		$aluno3 = "reprovado";
+	}else if($media3 < 7.0){
+		$aluno3 "Exame";
+	}else{
+		$aluno3 "Aprovado";
+	}
+
+
+	if ($media4< 3.0) {
+		$aluno4= "reprovado";
+	}else if($media4< 7.0){
+		$aluno4"Exame";
+	}else{
+		$aluno4 "Aprovado";
+	}
+
+
+
+
+	if ($media5 < 3.0) {
+		$aluno5 = "reprovado";
+	}else if($media5 < 7.0){
+		$aluno5 "Exame";
+	}else{
+		$aluno5 "Aprovado";
+	}
+
+
+	if ($media6 < 3.0) {
+		$aluno6 = "reprovado";
+		
+	}else if($media6 < 7.0){
+		$aluno6 "Exame";
+	}else{
+		$aluno6 "Aprovado";
+	}
+
+
+	$classMedia = ($media1 + $media2 + $media3 + $media4 +   $media5 + $media6)/6; 
+
 
 	
+
+
+
+
+
+
+	
+
+
+
+
  ?>
